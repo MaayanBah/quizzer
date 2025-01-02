c41d2b037ae49fba315e72e98a07842b33e9f52f
maayan <maayan@gmail.com>
2025-01-02 13:46:59 +0200

    Update core user

---
+++ core/serializers.py
@@ -0,0 +1,30 @@
+from quiz.models import QuizUser
+from djoser.serializers import (
+    UserSerializer as BaseUserSerializer,
+    UserCreateSerializer as BaseUserCreateSerializer,
+)
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = [
+            "id",
+            "username",
+            "password",
+            "email",
+            "first_name",
+            "last_name",
+        ]
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "date_joined",
+            "date_joined",
+        ]
