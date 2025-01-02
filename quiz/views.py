c7f778c60c189d63d85c5c605cd5a5e122e93397
Your Name <maayanbah@gmail.com>
2025-01-02 13:52:09 +0200

    Add QuizUser view set and user creation signal
    - Added QuizUser view set to handle quiz-related user operations.
    - Implemented a signal to automatically create a QuizUser instance
      when a core User is created.
    - Configured auth URLs for Djoser to integrate with user authentication.
    - Updated Djoser and Django REST Framework settings for authentication.

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import QuizUser, Category, Quiz, QuizResult, Question, Answer
+from .serializers import (
+    QuizUserSerializer,
+    CategorySerializer,
+    QuizSerializer,
+    QuestionSerializer,
+    AnswerSerializer,
+)
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import (
+    IsAdminUser,
+    IsAuthenticated,
+)
+
+
+class QuizUserViewSet(ModelViewSet):
+    queryset = QuizUser.objects.all()
+    serializer_class = QuizUserSerializer
+    permission_classes = [IsAdminUser]
+
+    @action(detail=False, methods=["GET", "PUT"], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        quiz_user = QuizUser.objects.get(user_id=request.user.id)
+        if request.method == "GET":
+            serializer = QuizUserSerializer(quiz_user)
+            return Response(serializer.data)
+        elif request.method == "PUT":
+            serializer = QuizUserSerializer(quiz_user, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
--- quizzer/urls.py
+++ quizzer/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+]
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("quiz/", include("quiz.urls")),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
 ]
