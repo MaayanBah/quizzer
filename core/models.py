ea879ea3a849ba15f2170a1da489840d5cca4867
Your Name <maayanbah@gmail.com>
2025-01-02 13:46:59 +0200

    Update core user

--- core/models.py
+++ core/models.py
@@ -1,12 +1,7 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractBaseUser):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
-    is_staff = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
 
-    USERNAME_FIELD = 'username'
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
