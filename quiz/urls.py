b2d7b4f6926b897b940d842f13799edc5e9396ff
maayan <maayan@gmail.com>
2025-01-05 17:38:56 +0200

    Add thw question API

--- quiz/urls.py
+++ quiz/urls.py
@@ -7,4 +7,8 @@ router.register("quiz_users", views.QuizUserViewSet)
 router.register("categories", views.CategoryViewSet)
 router.register("quiz", views.QuizViewSet)
 
-urlpatterns = router.urls
+
+quiz_router = routers.NestedDefaultRouter(router, "quiz", lookup="quiz")
+quiz_router.register("questions", views.QuestionViewSet, basename="quiz-questions")
+
+urlpatterns = router.urls + quiz_router.urls
