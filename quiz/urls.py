722e31b921bec17649456a093d0547da85bff108
maayan <maayan@gmail.com>
2025-01-05 17:09:59 +0200

    Implement the categories API

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,6 +4,7 @@ from . import views
 router = routers.DefaultRouter()
 
 router.register("quiz_users", views.QuizUserViewSet)
+router.register("categories", views.CategoryViewSet)
 router.register("quiz", views.QuizViewSet)
 
 urlpatterns = router.urls
